e9632e2d1ee4c61a16cbfa302a5bec3123935aca
charlenne alexander <charlennep@gmail.com>
2020-03-15 21:08:20 -0500

    it is getting the random location and showing on the console. needs to figure out how to shoe onto the page and attached a random uv

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,33 @@ console.log(showtime);
 
 // key and queryURL fro city
 var key = "5558573b04f9181b5515a2cc0280e2a9";
+var queryURLBase =
+  "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + key;
 var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Nashville&appid=" +
-  key;
+  "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=5558573b04f9181b5515a2cc0280e2a9";
+
+console.log(queryURLBase);
 console.log(queryURL);
 
 // key and queryURL for UV index
 
-var keyUV = "057e14341c48286f9140a48d5cf0795f";
+// var keyUV = "057e14341c48286f9140a48d5cf0795f";
 
-var queryURLuv =
-  "https://api.openweathermap.org/data/2.5/uvi?f&lat=36.17&lon=-86.78&appid=" +
-  keyUV;
-console.log(queryURLuv);
+// var queryURLuv =
+//   "https://api.openweathermap.org/data/2.5/uvi?f&lat=36.17&lon=-86.78&appid=" +
+//   keyUV;
+// console.log(queryURLuv);
 
-// funciton to set the time
+// function to set the time
 function setTime() {
-  var time = moment().format("MMMM Do YYYY, h:mm a");
+  var time = moment().format("LLLL");
   return time;
 }
 console.log(setTime());
 
+var queryTerm = " ";
+
+// function runQuery(queryURL) {
 // making the ajax call for weather
 $.ajax({
   url: queryURL,
@@ -38,34 +43,43 @@ $.ajax({
   $("#container").append(newDiv);
 
   var newForm = $(`<form class="form-inline my-2 my-lg-0" id= "form">
-    <input
-      class="form-control mr-sm-2"
-      type="search"
-      placeholder="Search"
-      aria-label="Search"
-      id= "input"
-    />
-    <button
-      class="btn btn-outline-dark my-2 my-sm-0"
-      type="submit"
-      id="btn"
-    ><i class= "fa fa-question-circle">
-      Search
-    </button>
-  </form>`);
+      <input
+        class="form-control mr-sm-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        id= "input"
+      />
+      <button
+        class="btn btn-outline-dark my-2 my-sm-0"
+        type="submit"
+        id="btn"
+      ><i class= "fa fa-question-circle"></i>
+        Search
+      </button>
+    </form>`);
   $(newDiv).append(newForm);
   $("#input").append("#btn");
   console.log(newForm);
 
   $("#btn").on("click", function(e) {
+    console.log("you click me");
     e.preventDefault();
 
+    var queryTerm = $("#input")
+      .val()
+      .trim();
+    console.log(queryTerm);
+
+    var newURL = queryURLBase + "&q" + queryTerm;
+    console.log(newURL);
+
     var guestInput = `<h1 id="city"></h1>
-    <h5 id="date"></h5>
-    <h5 id="temperature"></h5>
-    <h5 id="humidity"></h5>
-    <h5 id="wind"></h5>
-    <h5 id="uv"></h5>`;
+      <h5 id="date"></h5>
+      <h5 id="temperature"></h5>
+      <h5 id="humidity"></h5>
+      <h5 id="wind"></h5>
+       <h5 id="uv"></h5>`;
 
     $("#weatherBox").append(guestInput);
 
@@ -87,13 +101,14 @@ $.ajax({
     console.log("Temperature (F): " + tempF);
 
     // making ajax call for uv
-    $.ajax({
-      url: queryURLuv,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      $("#uv").text("UV" + " " + response.value);
-      console.log(response.value);
-    });
+    //     $.ajax({
+    //       url: queryURLuv,
+    //       method: "GET"
+    //     }).then(function(response) {
+    //       console.log(response);
+    //       $("#uv").text("UV" + " " + response.value);
+    //       console.log(response.value);
+    //     });
   });
 });
+// }
